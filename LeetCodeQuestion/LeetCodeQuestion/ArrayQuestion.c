d6b5084fc7057bcc9ccfd6ade34f51edda4d24fb
guoyf <guoyf@autoai.com>
2020-02-08 23:17:08 +0800

    提交每日一题第四题

--- LeetCodeQuestion/LeetCodeQuestion/ArrayQuestion.c
+++ LeetCodeQuestion/LeetCodeQuestion/ArrayQuestion.c
@@ -59,3 +59,47 @@ void reverse(int * nums,int start,int end){
         nums[end--]=temp;
     }
 }
+
+
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    
+    //解题思路1：
+    //拿到 两个的总数
+    int i = m-- + n-- -1;
+    //循环设置最后一个数
+    while(i >= 0){
+        if(m>=0 && n >= 0)
+            nums1[i--] = nums1[m] > nums2[n]? nums1[m--]:nums2[n--];
+        
+        else if(m < 0)
+            nums1[i--] = nums2[n--];
+        
+        else
+            break;
+    }
+    
+    
+    //解析思路2
+    //遍历数组2
+    for(int i=0;i<n;i++){
+        //拿到数组1的个数
+        int j=m-1;
+        
+        //从后向前遍历数组1
+        for(;j>=0;j--){
+            
+            //如果数组1的数大于，数组1的元素后移1位
+            if(nums2[i]<nums1[j]){
+                nums1[j+1]=nums1[j];
+            }else{
+                //找到了对应的位置j
+                break;
+            }
+        }
+        //将对应位置的值填入
+        nums1[j+1]=nums2[i];
+        //更新总数
+        m++;
+    }
+}
